0ed24186733c9604960dc1b7c01a9b7403dd1b8b
elandeholm <emanuel.landeholm@gmail.com>
2017-05-18 18:14:01 +0200

    no longer sieving even numbers

--- eratosthenes.h
+++ eratosthenes.h
@@ -4,8 +4,8 @@
 #define BITSTYPE_BITS (8 * BITSTYPE_SIZE)
 #define BITSTYPE_MASK (BITSTYPE_BITS - 1)
 
-#define BITVAL(n) ((BITSTYPE)1) << ((n) & BITSTYPE_MASK)
-#define SIEVE_PTR(sieve, n) *((sieve) + (n) / BITSTYPE_BITS)
+#define BITVAL(n) ((BITSTYPE)1) << (((n) >> 1) & BITSTYPE_MASK)
+#define SIEVE_PTR(sieve, n) *((sieve) + ((n) >> 1) / BITSTYPE_BITS)
 
 #define MARK_COMPOSITE(sieve, n) (SIEVE_PTR(sieve, n) |= BITVAL(n))
 #define TEST_COMPOSITE(sieve, n) (SIEVE_PTR(sieve, n) &  BITVAL(n))
